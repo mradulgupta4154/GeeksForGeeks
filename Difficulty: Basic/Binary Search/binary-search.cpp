abbbe8989f274742d6db419c20ee5ac83f773026
Mradul gupta <mradulgupta387a@gmail.com>
2026-09-13 14:17:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Binary Search/binary-search.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    bool binarySearch(vector<int>& arr, int k) {
+        int low = 0;
+        int high = arr.size() - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == k) {
+                return true;
+            } else if (arr[mid] < k) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return false;
+    }
+};
